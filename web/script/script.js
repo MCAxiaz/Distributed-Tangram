c347b00e0a7a93570e69eac3fc66cd74f3b12442
Eddie Zhou <eddiezhou1996@gmail.com>
2018-04-11 18:49:56 -0700

    Control click to drop or lock tans

--- web/script/script.js
+++ web/script/script.js
@@ -18,7 +18,7 @@ function renderTan(model, node) {
     var id = `tan-${model.id}`
     var transform = `translate(${model.location.x}, ${model.location.y}) rotate(${model.rotation})`;
     var d = "";
-    model.shape.points.forEach(function(point, i) {
+    model.shape.points.forEach(function (point, i) {
         var command = i == 0 ? "M" : "L";
         d += `${command} ${point.x} ${point.y} `;
     });
@@ -27,17 +27,17 @@ function renderTan(model, node) {
     node.id = id;
     node.setAttribute('fill', model.shape.fill);
     // Check if tan is being held by a player, and if it is, show that player's ID on it
-    if (!state.tans[model.id-1]) {
+    if (!state.tans[model.id - 1]) {
         console.log("No such tan.");
     }
     var txtPath = document.getElementById(`txtPath-tan-${model.id}`);
-    if (state.tans[model.id-1].player !== NO_PLAYER) {
+    if (state.tans[model.id - 1].player !== NO_PLAYER) {
         // Render player ID to tan
         if (!txtPath) {
             console.log(`textPath for tan ${model.id} does not exist.`);
         } else {
             node.setAttribute("fill-opacity", "0.5");
-            txtPath.innerHTML = state.tans[model.id-1].player;
+            txtPath.innerHTML = state.tans[model.id - 1].player;
         }
     } else {
         node.setAttribute("fill-opacity", "1");
@@ -49,12 +49,12 @@ function renderTan(model, node) {
     }
     node.setAttribute('stroke', model.shape.stroke);
     if (model.Matched) {
-      node.setAttribute('stroke', 'green');
+        node.setAttribute('stroke', 'green');
     }
 
     node.setAttribute('transform', transform);
     node.setAttribute('d', d);
-    node.setAttribute('class', 'draggable');
+    node.classList.add("tan");
     return node
 }
 
@@ -70,7 +70,7 @@ function attachPlayerNameTextToSVG(tanID) {
 
     var txtPath = document.createElementNS(view.namespaceURI, "textPath");
     txtPath.setAttribute("href", `#${tanID}`);
-    
+
     txtPath.id = `txtPath-${tanID}`;
     txtPath.innerHTML = "";
 
@@ -82,7 +82,7 @@ function attachPlayerNameTextToSVG(tanID) {
 function renderTargetTan(model, offset, node) {
     var transform = `translate(${model.location.x + offset.x}, ${model.location.y + offset.y}) rotate(${model.rotation})`;
     var d = "";
-    model.shape.points.forEach(function(point, i) {
+    model.shape.points.forEach(function (point, i) {
         var command = i == 0 ? "M" : "L";
         d += `${command} ${point.x} ${point.y} `;
     });
@@ -101,21 +101,21 @@ var socket;
 var config;
 var state;
 var player;
-document.addEventListener("DOMContentLoaded", function(e) {
+document.addEventListener("DOMContentLoaded", function (e) {
     var view = document.getElementById("view");
     var timer = document.getElementById("timer");
     var dump = document.getElementById("dump");
 
     function getTan(model) {
         var tan = view.getElementById(`tan-${model.id}`);
-        
+
         if (!tan) {
             tan = document.createElementNS(view.namespaceURI, "path");
             renderTan(model, tan);
             attachPlayerNameTextToSVG(tan.id);
             var gPath = document.getElementById("g-paths");
             gPath.appendChild(tan);
-            tan.addEventListener("pointerdown", onMouseDown)
+            tan.addEventListener("click", onMouseDown)
         }
         return tan;
     }
@@ -140,7 +140,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
         }
 
         // Check if tan is already held by someone else
-        var tanObj = state.tans[tanID-1];
+        var tanObj = state.tans[tanID - 1];
         if (!tanObj) {
             console.log("Tan does not exist.");
             return false;
@@ -152,7 +152,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
         } else {
             tanObj.player = player.ID;
         }
-        
+
         // Change path's fill opacity to half
         tan.setAttribute("fill-opacity", "0.5");
 
@@ -162,9 +162,11 @@ document.addEventListener("DOMContentLoaded", function(e) {
             console.log(`No such txtPath with tan ${tanID}.`);
             return false;
         }
-        
+
         txtPath.innerHTML = player.ID;
-        
+        tan.classList.add("locked");
+        tan.classList.add("draggable");
+
         console.log(`[Lock tan] Tan ${tanID}: I am possessed by ${player.ID}.`);
 
         return true;
@@ -179,7 +181,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
         }
 
         // Set tan to unlocked
-        var tanObj = state.tans[tanID-1];
+        var tanObj = state.tans[tanID - 1];
         if (!tanObj) {
             console.log("There exists no such tan.");
             return false;
@@ -189,7 +191,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
 
         // Restore path's fill opacity to 1
         tan.setAttribute("fill-opacity", "1");
-        
+
         // Remove player name from tan
         var txtPath = document.getElementById(`txtPath-tan-${tanID}`);
         if (!txtPath) {
@@ -198,6 +200,8 @@ document.addEventListener("DOMContentLoaded", function(e) {
         }
 
         txtPath.innerHTML = "";
+        tan.classList.remove("locked");
+        tan.classList.remove("draggable");
 
         console.log(`[Unlock tan] ${tanID}`);
 
@@ -206,12 +210,12 @@ document.addEventListener("DOMContentLoaded", function(e) {
     }
 
     function renderTarget(config) {
-      for (let ttan of config.targets) {
-        let node = document.createElementNS(view.namespaceURI, "path");
-        var gTarget = document.getElementById("g-target");
-        renderTargetTan(ttan, config.Offset, node)
-        gTarget.appendChild(node);
-      }
+        for (let ttan of config.targets) {
+            let node = document.createElementNS(view.namespaceURI, "path");
+            var gTarget = document.getElementById("g-target");
+            renderTargetTan(ttan, config.Offset, node)
+            gTarget.appendChild(node);
+        }
     }
 
     function renderGroups() {
@@ -228,68 +232,43 @@ document.addEventListener("DOMContentLoaded", function(e) {
     }
 
     socket = openSocket();
-    socket.addEventListener("message", function(e) {
+    socket.addEventListener("message", function (e) {
         dump.innerHTML = e.data
         var message = JSON.parse(e.data)
         switch (message.type) {
-        case "state":
-            state = message.data
-            render(state);
-            break;
-        case "config":
-            config = message.data;
-            view.setAttribute("width", config.Size.x)
-            view.setAttribute("height", config.Size.y)
-            renderGroups();
-            renderTarget(config);
-            break;
-        case "player":
-            player = message.data;
-            var currentPlayerID = document.getElementById("current-player-id");
-            currentPlayerID.innerHTML = player.ID;
-            break;
+            case "state":
+                state = message.data
+                render(state);
+                break;
+            case "config":
+                config = message.data;
+                view.setAttribute("width", config.Size.x)
+                view.setAttribute("height", config.Size.y)
+                renderGroups();
+                renderTarget(config);
+                break;
+            case "player":
+                player = message.data;
+                var currentPlayerID = document.getElementById("current-player-id");
+                currentPlayerID.innerHTML = player.ID;
+                break;
         }
     });
-    socket.addEventListener("open", function(e) {
+    socket.addEventListener("open", function (e) {
         socket.send(JSON.stringify({
-            type:"GetState"
+            type: "GetState"
         }));
     })
 
-    setInterval(function() {
+    setInterval(function () {
         if (state) {
             var d = Date.now() - new Date(state.Timer).getTime()
             timer.innerHTML = Math.round(d / 1000)
         }
     }, 100)
 
-    function onMouseDown(e) {
-        var path = e.target;
-        var id = parseInt(path.id.match(/tan-(\d+)/)[1]);
-
-        var tan = state.tans.find(function(tan) {
-            return tan.id == id
-        });
-
-        var locked = lockTan(tan.id);
-        if (!locked) {
-            return;
-        }
-
-        console.log(`Holding on to tan id=${id}`);
-        
-        var startTanPos = {
-            x: tan.location.x,
-            y: tan.location.y,
-            r: tan.rotation
-        };
-
-        var startMousePos = {
-            x: e.clientX,
-            y: e.clientY
-        };
-
-        var mouseMoveListener = function (e) {
+    function mouseMoveListener(tan, path, startTanPos, startMousePos) {
+        return (e) => {
             tan.location.x = Math.round(Math.max(0, Math.min(startTanPos.x + (e.clientX - startMousePos.x), config.Size.x)));
             tan.location.y = Math.round(Math.max(0, Math.min(startTanPos.y + (e.clientY - startMousePos.y), config.Size.y)));
             renderTan(tan, path);
@@ -299,18 +278,19 @@ document.addEventListener("DOMContentLoaded", function(e) {
                 location: tan.location,
                 rotation: tan.rotation
             }));
-        };
-        document.addEventListener("pointermove", mouseMoveListener);
+        }
+    };
 
-        // Rotate tan clockwise or counter-clockwise
-        var rotateListener = function (e) {
-            var key = e.which;
-            var d = 0;
+    // Rotate tan clockwise or counter-clockwise
+    function rotateListener (tan, path) {
+        return (e) => {
+            const key = e.which;
+            let d = 0;
             switch (key) {
-            case 88:
+                case 88:
                 d = 1;
                 break;
-            case 90:
+                case 90:
                 d = -1
                 break;
             }
@@ -319,40 +299,79 @@ document.addEventListener("DOMContentLoaded", function(e) {
                 tan.rotation = rotate(tan.rotation, d);
                 renderTan(tan, path);
             }
+
             socket.send(JSON.stringify({
                 type: "MoveTan",
                 tan: tan.id,
                 location: tan.location,
                 rotation: tan.rotation
             }));
-        };
-        document.addEventListener("keydown", rotateListener);
-
-        document.addEventListener("pointerup", function(e) {
-            var unlock = unlockTan(id);
-            if (!unlock) {
-                console.log(`Error encountered while unlocking tan ${id}`);
-            }
-            console.log(`Releasing tan id=${id}`);
-            document.removeEventListener("pointermove", mouseMoveListener);
-            document.removeEventListener("keydown", rotateListener);
-
-            socket.send(JSON.stringify({
-                type: "ObtainTan",
-                tan: id,
-                release: true
-            }));
-        }, {
-            once:true
-        });
-
-        socket.send(JSON.stringify({
-            type: "ObtainTan",
-            tan: id,
-            release: false
-        }));
+        }
     }
-})
+
+    const handlers = {};
+
+    function onMouseDown(e) {
+        if (e.ctrlKey) {
+            var path = e.target;
+            var id = parseInt(path.id.match(/tan-(\d+)/)[1]);
+
+            var tan = state.tans.find(function (tan) {
+                return tan.id == id
+            });
+
+            if (state.tans[id - 1].player === player.ID) {
+                var unlock = unlockTan(id);
+                if (!unlock) {
+                    console.log(`Error encountered while unlocking tan ${id}`);
+                }
+                console.log(`Releasing tan id=${id}`);
+
+                if (handlers[id] && handlers[id].move && handlers[id].rotate) {
+                    document.removeEventListener("pointermove", handlers[id].move);
+                    document.removeEventListener("keydown", handlers[id].rotate);
+                }
+
+                socket.send(JSON.stringify({
+                    type: "ObtainTan",
+                    tan: id,
+                    release: true
+                }));
+            } else {
+                var locked = lockTan(tan.id);
+                if (!locked) {
+                    return;
+                }
+
+                console.log(`Holding on to tan id=${id}`);
+                const startTanPos = {
+                    x: tan.location.x,
+                    y: tan.location.y,
+                    r: tan.rotation
+                };
+
+                const startMousePos = {
+                    x: e.clientX,
+                    y: e.clientY
+                };
+
+                handlers[id] = {
+                    move: mouseMoveListener(tan, path, startTanPos, startMousePos),
+                    rotate: rotateListener(tan, path)
+                };
+
+                document.addEventListener("pointermove", handlers[id].move);
+                document.addEventListener("keydown", handlers[id].rotate);
+
+                socket.send(JSON.stringify({
+                    type: "ObtainTan",
+                    tan: id,
+                    release: false
+                }));
+            }
+        }
+    }
+});
 
 function rotate(r, d) {
     return (r + d * 15 + 720) % 360;
